57d47966f18ae0ae8e6d0f9737fb3c7bee2f559a
jbinion <jbinion0366@gmail.com>
2025-08-06 12:33:28 -0400

    fix: styling #3

--- app/src/routes/data/+page.svelte
+++ app/src/routes/data/+page.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
-	import capitalizeFirstLetter from '$lib/capitalizeFirstLetter';
 	import episodes from '../../../../episodes.json';
 </script>
 
-<div class="flex flex-col space-y-12 px-12 pb-12">
+<div class="container mx-auto flex flex-col space-y-12 px-12 pb-12 pt-12">
+	<div class="text-start">
+		<a href="/">Back</a>
+	</div>
+	<p class="text-start">All Episodes</p>
 	<div class="grid grid-cols-1">
 		{#each episodes as episode, index}
 			<div
-				class="flex flex-row items-center gap-4 border-zinc-300 py-4"
+				class="flex flex-row items-center gap-4 py-4"
 				class:border-b={index < episodes.length - 1 &&
 					episodes[index + 1].season !== episode.season}
 			>
-				<p class="w-48 text-center text-xl font-bold">s{episode.season}:{episode.number}</p>
+				<p class="w-48 text-xl font-bold">s{episode.season}:{episode.number}</p>
 
 				<div class="text-start">
 					<p>{episode.name}</p>
 					{#if episode.startTime !== '' && episode.startDay !== ''}
-						<p class="font-light text-zinc-300">
-							{episode.startTime} on a {capitalizeFirstLetter(episode.startDay)}
-						</p>
+						<p class="font-light">{episode.startTime} on a {episode.startDay}</p>
 					{/if}
 				</div>
 			</div>
