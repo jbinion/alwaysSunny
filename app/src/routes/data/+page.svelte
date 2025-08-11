86debc0bc85039b799161107877d12edf083e02e
jbinion <jbinion0366@gmail.com>
2025-08-11 15:33:35 -0400

    fix: styling

--- app/src/routes/data/+page.svelte
+++ app/src/routes/data/+page.svelte
@@ -3,25 +3,22 @@
 	import episodes from '../../../../episodes.json';
 </script>
 
-<div class="flex flex-col space-y-12 px-12 pb-12">
-	<div class="grid grid-cols-1">
-		{#each episodes as episode, index (episode.name)}
-			<div
-				class="flex flex-row items-center gap-4 border-zinc-300 py-4"
-				class:border-b={index < episodes.length - 1 &&
-					episodes[index + 1].season !== episode.season}
-			>
-				<p class="w-48 text-center text-xl font-bold">s{episode.season}:{episode.number}</p>
+<div class="flex flex-col space-y-4 px-12 pb-12">
+	{#each episodes as episode, index (episode.name)}
+		<div
+			class="flex flex-row items-center gap-8 border-zinc-300 py-4 md:gap-12"
+			class:border-b={index < episodes.length - 1 && episodes[index + 1].season !== episode.season}
+		>
+			<p class="w-[48px] text-xl font-bold">s{episode.season}:{episode.number}</p>
 
-				<div class="text-start">
-					<p>{episode.name}</p>
-					{#if episode.startTime !== '' && episode.startDay !== ''}
-						<p class="font-light text-zinc-300">
-							{episode.startTime} on a {capitalizeFirstLetter(episode.startDay)}
-						</p>
-					{/if}
-				</div>
+			<div class="">
+				<p>{episode.name}</p>
+				{#if episode.startTime !== '' && episode.startDay !== ''}
+					<p class="font-light text-zinc-300">
+						{episode.startTime} on a {capitalizeFirstLetter(episode.startDay)}
+					</p>
+				{/if}
 			</div>
-		{/each}
-	</div>
+		</div>
+	{/each}
 </div>
