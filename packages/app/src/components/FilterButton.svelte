1e974341c3ae50e7b8deebdbae7eb4348e1f6a2d
jbinion <jbinion0366@gmail.com>
2025-09-10 23:57:02 -0400

    refactor: extract filter button & add styling

---
+++ packages/app/src/components/FilterButton.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let active = false;
+	export let handleClick: () => void;
+	export let title: string;
+</script>
+
+<button on:click={handleClick} class="filterButton" class:active>{title}</button>
